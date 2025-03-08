0d13b779c84199f4e55ad62731e4423d8db5dd65
Udeme Udeme <darealudeme1@gmail.com>
2025-03-08 05:30:00 +0100

    Intro - React overview

--- src/Intro/index.jsx
+++ src/Intro/index.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Intro = () => {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    setCount((prev) => prev + 1);
   }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
   );
+
+  function Message(props) {
+    return <p>You have {props.count} pieces of advice</p>;
+  }
 };
 
 export default Intro;
