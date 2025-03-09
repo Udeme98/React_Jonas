736a80015f3da9722d532ec82099a4f20cb241c6
Udeme Udeme <darealudeme1@gmail.com>
2025-03-09 21:40:07 +0100

    Pizza Menu - props

--- src/Pizza Menu/Menu.jsx
+++ src/Pizza Menu/Menu.jsx
@@ -4,19 +4,37 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Focaccia"
+        photoImage="/public/pizzaImages/focaccia.jpg"
+        ingerdients="Bread with italian olive oil and rosemary"
+        price={6}
+      />
+      <Pizza
+        name="Funghi"
+        photoImage="/public/pizzaImages/funghi.jpg"
+        ingerdients="Bread with funghi"
+        price={4}
+      />
+      <Pizza
+        name="Spinaci"
+        photoImage="/public/pizzaImages/spinaci.jpg"
+        ingerdients="Tomato, mozarella, spinach, and ricotta cheese"
+        price={7}
+      />
     </main>
   );
 };
-const Pizza = () => {
+const Pizza = (props) => {
   return (
-    <>
-      <img src="/public/pizzaImages/focaccia.jpg" />
-      <h3>Focaccia</h3>
-      <p>Tomato, Mozarella, Spinach, and Ricotta cheese</p>
-    </>
+    <div className="pizza">
+      <img src={props.photoImage} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingerdients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 };
 
