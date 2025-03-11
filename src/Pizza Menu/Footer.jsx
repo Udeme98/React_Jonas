9ca06af71f2edd5dde83f37122bcd2ea4b3843e5
Udeme Udeme <darealudeme1@gmail.com>
2025-03-11 09:59:35 +0100

    Pizza Menu - destructuring props

--- src/Pizza Menu/Footer.jsx
+++ src/Pizza Menu/Footer.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 const Footer = () => {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = "08";
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -23,3 +20,15 @@ const Footer = () => {
 };
 
 export default Footer;
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 until {closeHour}:00. Come visit us or
+        order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
--- src/Pizza Menu/Menu.jsx
+++ src/Pizza Menu/Menu.jsx
@@ -24,16 +24,16 @@ const Menu = () => {
   );
 };
 
-const Pizza = (props) => {
-  if (props.pizzaObj.soldOut) return null;
+const Pizza = ({ pizzaObj }) => {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} />
+      <img src={pizzaObj.photoName} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
