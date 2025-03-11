cdd11cd1a7bf837ffa54821471fa34e2d4ef91e1
Udeme Udeme <darealudeme1@gmail.com>
2025-03-11 09:24:38 +0100

    Pizza Menu - conditional rendering with ?

--- src/Pizza Menu/Menu.jsx
+++ src/Pizza Menu/Menu.jsx
@@ -11,12 +11,14 @@ const Menu = () => {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => {
             return <Pizza pizzaObj={pizza} key={pizza.name} />;
           })}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please com back later :)</p>
       )}
     </main>
   );
