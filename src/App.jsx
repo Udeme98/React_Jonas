5b8e1e9ac55f76e827e8ed5b5129cd1c646e9f70
Udeme Udeme <darealudeme1@gmail.com>
2025-03-12 09:54:40 +0100

    file/folder reshuffle

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Intro from "./Intro";
-import PizzaMenu from "./Pizza Menu";
-import ProfileCard from "./Profile card";
 
 const App = () => {
-  return (
-    <div>
-      {/* <Intro /> */}
-      {/* <PizzaMenu /> */}
-      <ProfileCard />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
