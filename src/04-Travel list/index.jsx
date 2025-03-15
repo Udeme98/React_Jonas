c98c6c9592416a956404bdc9c483c46604211c12
Udeme Udeme <darealudeme1@gmail.com>
2025-03-15 20:29:59 +0100

    Travel list - Forms and handling submissions

--- src/04-Travel list/index.jsx
+++ src/04-Travel list/index.jsx
@@ -4,6 +4,11 @@ import "./travel.css";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
+  { id: 4, description: "Socks", quantity: 12, packed: true },
+  { id: 5, description: "Socks", quantity: 12, packed: true },
+  { id: 6, description: "Socks", quantity: 12, packed: true },
+  { id: 7, description: "Socks", quantity: 12, packed: true },
 ];
 
 const Logo = () => {
@@ -11,10 +16,23 @@ const Logo = () => {
 };
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 };
 
@@ -23,7 +41,7 @@ const ParkingList = () => {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
