e2b6c50e46830f0563ee47e0883e4c898ebce16f
Udeme Udeme <darealudeme1@gmail.com>
2025-04-03 10:21:08 +0100

    Eat-and-split: mapping

--- src/04-Eat-and-Split/index.jsx
+++ src/04-Eat-and-Split/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./eat.css";
+import initialFriends from "./friends";
+// console.log(initialFriends);
 
 const Eat = () => {
-  return <div>Eat-and-Split</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList friends={initialFriends} />
+      </div>
+    </div>
+  );
 };
-
 export default Eat;
+
+function FriendsList({ friends }) {
+  const friendss = friends;
+
+  return (
+    <ul>
+      {friendss.map((friend) => (
+        <li>{friend.name}</li>
+      ))}
+    </ul>
+  );
+}
