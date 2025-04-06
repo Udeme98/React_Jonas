2ae76439d8c6cf069669d4f8c6eb2195bc510bb2
Udeme Udeme <darealudeme1@gmail.com>
2025-04-06 08:08:12 +0100

    Eat-and-split: Building the static app list of friends

--- src/04-Eat-and-Split/index.jsx
+++ src/04-Eat-and-Split/index.jsx
@@ -20,8 +20,32 @@ function FriendsList({ friends }) {
   return (
     <ul>
       {friendss.map((friend) => (
-        <li key={friend.name}>{friend.name}</li>
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.img} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p className="red">You and {friend.name} are even.</p>
+      )}
+      <button className="button">Select</button>
+    </li>
   );
 }
