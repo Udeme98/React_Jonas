1bd475f5d25ea54f5aeb59f24251b3705b3115e3
Udeme Udeme <darealudeme1@gmail.com>
2025-04-06 13:45:15 +0100

    Eat-and-split: Building the static app forms

--- src/04-Eat-and-Split/index.jsx
+++ src/04-Eat-and-Split/index.jsx
@@ -8,7 +8,10 @@ const Eat = () => {
     <div className="app">
       <div className="sidebar">
         <FriendsList friends={initialFriends} />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 };
@@ -42,10 +45,49 @@ function Friend({ friend }) {
           {friend.name} owes you ${Math.abs(friend.balance)}
         </p>
       )}
-      {friend.balance === 0 && (
-        <p className="red">You and {friend.name} are even.</p>
-      )}
-      <button className="button">Select</button>
+      {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👯 Friend name</label>
+      <input type="text" />
+
+      <label>Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🙂 Your expenses</label>
+      <input type="text" />
+
+      <label>👯 X's expenses</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
