f8675ca0a83d1e845e8cdc2b72d1490b5a97173b
Udeme Udeme <darealudeme1@gmail.com>
2025-04-17 07:29:15 +0100

    Challenge #1 - Text Expander

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import UsePopcorn from "./05-usePopcorn";
-import StarRating from "./05-usePopcorn/StarRating";
+
+import TextExpander from "./Challenges/Text Expander/TextExpander";
 
 const App = () => {
   return (
     <div>
-      {/* <UsePopcorn /> */}
-      <StarRating
-        maxRating={5}
-        messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      />
-      <StarRating
-        maxRating={5}
-        size={25}
-        color="purple"
-        className="test"
-        defaultRating={3}
-      />
+      <TextExpander />
     </div>
   );
 };
--- src/Challenges/Text Expander/TextExpander.jsx
+++ src/Challenges/Text Expander/TextExpander.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./textExpander.css";
+
+function TextExpander2() {
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander buttonInline={false} className="box">
+        Space mission have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
 
 const TextExpander = () => {
-  return <div>TextExpander</div>;
+  return <div>TODO</div>;
 };
 
 export default TextExpander;
