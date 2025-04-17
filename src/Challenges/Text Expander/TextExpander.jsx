552e5e97ac482de2a45a4977d1002187705281ff
Udeme Udeme <darealudeme1@gmail.com>
2025-04-17 09:40:20 +0100

    Challenge #1 - Text Expander

--- src/Challenges/Text Expander/TextExpander.jsx
+++ src/Challenges/Text Expander/TextExpander.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./textExpander.css";
 
-function TextExpander2() {
+function TextExpander() {
   return (
     <div>
-      <TextExpander>
+      <TextExpander2>
         Space travel is the ultimate adventure! Imagine soaring past the stars
         and exploring new worlds It's the stuff of dreams and science fiction,
         but believe it or not, space travel is a real thing. Humans and robots
         are constantly venturing out into the cosos to uncover its secrets and
         push the boundaries of what's possible.
-      </TextExpander>
+      </TextExpander2>
 
-      <TextExpander
+      <TextExpander2
         collapsedNumWords={20}
         expandButtonText="Show text"
         collapseButtonText="Collapse text"
@@ -23,19 +23,19 @@ function TextExpander2() {
         but believe it or not, space travel is a real thing. Humans and robots
         are constantly venturing out into the cosos to uncover its secrets and
         push the boundaries of what's possible.
-      </TextExpander>
+      </TextExpander2>
 
-      <TextExpander buttonInline={false} className="box">
+      <TextExpander2 buttonInline={false} className="box">
         Space mission have given us incredible insights into our universe and
         have inspired future generations to keep reaching for the stars. Space
         travel is a pretty cool thing to think about. Who knows what we'll
         discover next!
-      </TextExpander>
+      </TextExpander2>
     </div>
   );
 }
 
-const TextExpander = () => {
+const TextExpander2 = () => {
   return <div>TODO</div>;
 };
 
