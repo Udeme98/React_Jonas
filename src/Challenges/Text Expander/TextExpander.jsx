33cab7bd6f2677bcf375caca7a7301ca8dc72574
Udeme Udeme <darealudeme1@gmail.com>
2025-04-17 21:45:05 +0100

    Challenge #1 - Text Expander

--- src/Challenges/Text Expander/TextExpander.jsx
+++ src/Challenges/Text Expander/TextExpander.jsx
@@ -40,7 +40,7 @@ const TextExpander2 = ({
   collapsedNumWords = 10,
   expandButtonText = "Show more",
   collapseButtonText = "Show less",
-  buttonColor,
+  buttonColor = "#1f09cd",
   expanded = false,
   className,
   children,
@@ -50,10 +50,19 @@ const TextExpander2 = ({
     ? children
     : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
 
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor,
+  };
+
   return (
     <div className={className}>
       <span>{displayText}</span>
-      <button onClick={() => setIsExpanded((exp) => !exp)}>
+      <button onClick={() => setIsExpanded((exp) => !exp)} style={buttonStyle}>
         {isExpanded ? collapseButtonText : expandButtonText}
       </button>
     </div>
