f314773e1ebe29d7ee1cc0bec1f0fd4e0dca5d33
Udeme Udeme <darealudeme1@gmail.com>
2025-06-02 10:35:43 +0100

    Effects and Data-fetching

--- src/05-usePopcorn/index.jsx
+++ src/05-usePopcorn/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import "./popcorn.css";
 
 const tempMovieData = [
@@ -208,9 +208,11 @@ export default function UsePopcorn() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then((res) =>
-    res.json().then((data) => console.log(data.Search))
-  );
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then((res) =>
+      res.json().then((data) => setMovies(data.Search))
+    );
+  }, []);
 
   return (
     <>
